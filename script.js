b1e1b274d86911f01b523aa23e601d5d577b0f63
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-20 14:11:19 -0500

    with json file

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ searchInput.addEventListener("input", e => {
   })
 })
 
-fetch("userinfo.csv")
+fetch("userinfo.json")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
