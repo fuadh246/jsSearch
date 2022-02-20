f782ffe8d92f78788b1066c4d9e10658e9b643b3
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-20 12:02:42 -0500

    with csv file

--- script.js
+++ script.js
@@ -10,21 +10,25 @@ searchInput.addEventListener("input", e => {
   users.forEach(user => {
     const isVisible =
       user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+      user.email.toLowerCase().includes(value) 
+      //|| 
+     // user.id.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("userinfo.csv")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
+      const idnum = card.querySelector("[data-idnum]")
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
+      idnum.textContent = user.id
       header.textContent = user.name
       body.textContent = user.email
       userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      return {id:user.id, name: user.name, email: user.email, element: card }
     })
   })
