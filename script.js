00fe64da60cfbf8a1ba1f8edc3810fe43d6463a8
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-22 12:09:31 -0500

    with img

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
+const avatar = document.querySelector("[data-avatar]")
+const gender = document.querySelector("[data-gender]")
 const submit = document.getElementById('submit');
-const fileInput = document.querySelector("[fileinput]").value
 let users = []
 
 searchInput.addEventListener("input", e => {
@@ -16,21 +17,24 @@ searchInput.addEventListener("input", e => {
     user.element.classList.toggle("hide", !isVisible)
   })
 })
-submit.addEventListener("click", 
-  fetch("fileInput")
+//const fileInput = document.querySelector('fileInput');
+fetch("MOCK_DATA.json")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const idnum = card.querySelector("[data-idnum]")
+      const avatar = card.querySelector("[data-avatar]")
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
+      const gender = card.querySelector("[data-gender]")
+
       idnum.textContent = user.id
-      header.textContent = user.name
+      avatar.src=user.avatar
+      header.textContent = user.first_name
       body.textContent = user.email
+      gender.textContent=user.gender
       userCardContainer.append(card)
-      return {id:user.id, name: user.name, email: user.email, element: card }
+      return {id:user.id,avature: user.img, name: user.first_name, email: user.email, gender: user.gender, element: card }
     })
-  }))
-
-
+  })
